d75795b6e5a33eb8aa8f3a4381e57cd75a629653
tom caruso <carusot42@gmail.com>
2017-11-10 23:28:08 -0500

    fixed install python.sh

--- install_python.sh
+++ install_python.sh
@@ -2,7 +2,7 @@
 
 curl -o python-3.tar.xz  https://www.python.org/ftp/python/3.5.4/Python-3.5.4.tar.xz
 tar -xf python-3.tar.xz
-cd Python-3.6.2
+cd Python-3.5.4
 
 ./configure --enable-optimizations --with-lto
 make -j 8
