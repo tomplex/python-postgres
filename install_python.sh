d877fffbc8695624a704c6e715bca9a6c42ef2b5
tom caruso <carusot42@gmail.com>
2017-11-10 23:35:51 -0500

    python3.4

--- install_python.sh
+++ install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o python-3.tar.xz  https://www.python.org/ftp/python/3.5.4/Python-3.5.4.tar.xz
+curl -o python-3.tar.xz  https://www.python.org/ftp/python/3.4.6/Python-3.4.6.tar.xz
 tar -xf python-3.tar.xz
-cd Python-3.5.4
+cd Python-3.4.6
 
 ./configure --enable-optimizations --with-lto
 make -j 8
